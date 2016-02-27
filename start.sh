fa1e5f43c80567555d39de401de30eeda153087c
Dirk Stichling <mytracks@mytracks4mac.com>
2016-02-27 18:43:24 +0100

    initial checkin

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+trap "echo TRAPed signal" HUP INT QUIT KILL TERM
+
+su -c '/usr/pgsql-9.5/bin/pg_ctl -D /home/pgdata/osm start' postgres
+sleep 20
+
+if [ ! -f /etc/osm_import_complete ]; then
+  cd ~/osm2pgsql
+  curl -O http://download.geofabrik.de/europe/germany/hamburg-latest.osm.pbf
+  /usr/local/bin/osm2pgsql -m --slim --flat-nodes flat.nodes --drop --cache 10000 --number-processes 4 -d gis *.osm.* -U gis
+  rm flat.nodes
+  touch /etc/osm_import_complete
+fi
+
+sleep infinity
+
+# stop service and clean up here
+su -c '/usr/pgsql-9.5/bin/pg_ctl -D /home/pgdata/osm stop' postgres
+
+echo "exited $0"
