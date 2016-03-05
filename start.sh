f6f51987f8cd48ffeb068b0544de59fef6694628
Dirk Stichling <mytracks@mytracks4mac.com>
2016-03-05 10:17:45 +0100

    update

--- start.sh
+++ start.sh
@@ -3,18 +3,17 @@
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
 su -c '/usr/pgsql-9.5/bin/pg_ctl -D /home/pgdata/osm start' postgres
-sleep 20
+sleep 5 
 
 if [ ! -f /etc/osm_import_complete ]; then
-  cd ~/osm2pgsql
-  curl -O http://planet.osm.org/planet/planet-latest.osm.bz2
-  /usr/local/bin/osm2pgsql -m --slim --flat-nodes flat.nodes --drop --cache 6000 --number-processes 4 -d gis planet-latest.osm.bz2 -U gis
-  touch /etc/osm_import_complete
-  rm flat.nodes
-  rm planet-latest.osm.bz2
+  curl $OSM_FILE > file.osm.bz2 && /usr/local/bin/osm2pgsql -m --slim --flat-nodes flat.nodes --drop --cache 6000 --number-processes 4 -d gis file.osm.bz2 -U gis && touch /etc/osm_import_complete
+  rm -f flat.nodes
+  rm -f file.osm.bz2
 fi
 
-sleep infinity
+if [ -f /etc/osm_import_complete ]; then
+  sleep infinity
+fi
 
 # stop service and clean up here
 su -c '/usr/pgsql-9.5/bin/pg_ctl -D /home/pgdata/osm stop' postgres
